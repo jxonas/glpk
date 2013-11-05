f22dfc57d5150c8e619a04b6b953d221a68e5c36
Jonas Rodrigues <jxonas@gmail.com>
2013-11-05 18:15:32 +0100

    removed glp: prefix from test

--- glpk/test.rkt
+++ glpk/test.rkt
@@ -1,17 +1,17 @@
 #lang racket
 
-(require (prefix-in glp: glpk))
+(require glpk)
 
-(define lp (glp:create-prob))
-(glp:set-prob-name! lp "sample")
-(glp:set-obj-dir! lp glp:MAX)
+(define lp (create-prob))
+(set-prob-name! lp "sample")
+(set-obj-dir! lp MAX)
 
-(glp:add-rows! lp 3)
-(glp:set-row-name! lp 1 "p")
-(glp:set-row-bnds! lp 1 glp:UP 0.0 100.0)
-(glp:set-row-name! lp 2 "q")
-(glp:set-row-bnds! lp 2 glp:UP 0.0 600.0)
-(glp:set-row-name! lp 3 "r")
-(glp:set-row-bnds! lp 3 glp:UP 0.0 300.0)
+(add-rows! lp 3)
+(set-row-name! lp 1 "p")
+(set-row-bnds! lp 1 UP 0.0 100.0)
+(set-row-name! lp 2 "q")
+(set-row-bnds! lp 2 UP 0.0 600.0)
+(set-row-name! lp 3 "r")
+(set-row-bnds! lp 3 UP 0.0 300.0)
 
-(glp:add-cols! lp 3)
+(add-cols! lp 3)
